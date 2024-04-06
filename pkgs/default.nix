d5db90a8ff280045ec1ead0f8ab7500dff10f8ff
ozwaldorf <self@ossian.dev>
2024-04-06 00:02:31 -0400

    fix: lints on package overlay

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,39 +1,40 @@
 { inputs }:
 {
-  overlays.default = (
-    final: pkgs: {
-      # Fork with sway ipc service
-      ags = inputs.ags.packages.${pkgs.system}.default;
+  overlays.default = final: prev: {
+    # Fork with sway ipc service
+    ags = inputs.ags.packages.${prev.system}.default;
 
-      # Update upstream
-      sway-contrib.grimshot = pkgs.sway-contrib.grimshot.overrideAttrs (_: {
-        src = pkgs.fetchFromGitHub {
-          owner = "OctopusET";
-          repo = "sway-contrib";
-          rev = "b7825b218e677c65f6849be061b93bd5654991bf";
-          hash = "sha256-ZTfItJ77mrNSzXFVcj7OV/6zYBElBj+1LcLLHxBFypk=";
-        };
-      });
-
-      # Carburetor gtk theme (patched catpuccin-gtk)
-      carburetor-gtk = import ./carburetor-gtk.nix { inherit pkgs; };
-
-      # Webcord with arRPC bridge enabled on startup
-      webcord = import ./webcord.nix { inherit pkgs; };
-
-      # Patched to work with hyprland 0.37
-      wezterm = import ./wezterm.nix { inherit pkgs; };
-
-      # Standalone neovim configuration
-      neovim = import ./neovim.nix { inherit inputs pkgs; };
-
-      # Unified headless package with standalone versions of home packages
-      # Overrides pkgs with the final output of the overlay, so that the
-      # standalone neovim can be included in the outputs
-      standalone = import ./standalone.nix {
-        inherit inputs;
-        pkgs = final;
+    # Update upstream
+    sway-contrib.grimshot = prev.sway-contrib.grimshot.overrideAttrs (_: {
+      src = prev.fetchFromGitHub {
+        owner = "OctopusET";
+        repo = "sway-contrib";
+        rev = "b7825b218e677c65f6849be061b93bd5654991bf";
+        hash = "sha256-ZTfItJ77mrNSzXFVcj7OV/6zYBElBj+1LcLLHxBFypk=";
       };
-    }
-  );
+    });
+
+    # Carburetor gtk theme (patched catpuccin-gtk)
+    carburetor-gtk = import ./carburetor-gtk.nix { pkgs = prev; };
+
+    # Webcord with arRPC bridge enabled on startup
+    webcord = import ./webcord.nix { pkgs = prev; };
+
+    # Patched to work with hyprland 0.37
+    wezterm = import ./wezterm.nix { pkgs = prev; };
+
+    # Standalone neovim configuration
+    neovim = import ./neovim.nix {
+      inherit inputs;
+      pkgs = prev;
+    };
+
+    # Unified headless package with standalone versions of home packages
+    # Overrides pkgs with the final output of the overlay, so that the
+    # standalone neovim can be included in the outputs
+    standalone = import ./standalone.nix {
+      inherit inputs;
+      pkgs = final;
+    };
+  };
 }
