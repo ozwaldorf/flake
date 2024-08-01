ee1300913d58a0b499f14bdca98e7a2bda04e63d
ozwaldorf <self@ossian.dev>
2024-08-01 13:58:52 -0400

    feat: install horizontal servers css for vesktop

--- home/vesktop.nix
+++ home/vesktop.nix
@@ -1,10 +1,17 @@
 { pkgs, ... }:
 {
-  carburetor.themes.vesktop.enable = true;
   home.packages = [
     (pkgs.vesktop.override {
       withTTS = false;
       withSystemVencord = true;
     })
   ];
+
+  carburetor.themes.vesktop.enable = true;
+
+  # Install horizontal server list css
+  xdg.configFile."vesktop/themes/horizontal.css".source = builtins.fetchurl {
+    url = "https://betterdiscord.app/Download\?id=124";
+    sha256 = "0c9gmi6axjlk5w1ivdjnx2mz5qr9hc10f55gjmjaxy8lnwc7p9jc";
+  };
 }
