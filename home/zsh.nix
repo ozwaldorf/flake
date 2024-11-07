317417c0869ad3fe2585fea9b4f21abd93d12dd7
ozwaldorf <self@ossian.dev>
2024-11-07 14:08:21 -0500

    zsh: plugins to set title properly and vi line editor

--- home/zsh.nix
+++ home/zsh.nix
@@ -16,11 +16,24 @@
 
   programs.zsh = {
     enable = true;
-    # inline history
-    autosuggestion.enable = true;
+
+    history = {
+      save = 1000000;
+      ignoreAllDups = true;
+      ignoreSpace = true;
+      share = true;
+    };
+
+    # inline history suggestions
+    autosuggestion = {
+      enable = true;
+      highlight = "fg=244";
+    };
     syntaxHighlighting.enable = true;
+    autocd = true;
     enableVteIntegration = true;
     enableCompletion = false; # autocomplete will compinit on its own
+
     plugins = [
       {
         name = "zsh-autocomplete";
@@ -30,6 +43,20 @@
           rev = "23.07.13";
           sha256 = "0NW0TI//qFpUA2Hdx6NaYdQIIUpRSd0Y4NhwBbdssCs=";
         };
+      }
+      {
+        name = "zsh-window-title";
+        src = pkgs.fetchFromGitHub {
+          owner = "olets";
+          repo = "zsh-window-title";
+          rev = "v1.2.0";
+          sha256 = "RqJmb+XYK35o+FjUyqGZHD6r1Ku1lmckX41aXtVIUJQ=";
+        };
+      }
+      {
+        name = "vi-mode";
+        src = pkgs.zsh-vi-mode;
+        file = "share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
       }
     ];
 
@@ -56,59 +83,39 @@
       fuck = "thefuck";
       switch = "sudo nixos-rebuild switch";
     };
-    initExtraFirst = ''
-      fpath=("$HOME/.zsh/completions" $fpath)
 
+    sessionVariables = {
+      # Let's break up words more
+      WORDCHARS = "*?[]~=&;!#$%^(){}<>";
+      PATH = "$HOME/.deno/bin:$PATH";
+    };
+
+    initExtraFirst = ''
       ## Options section
+      setopt complete_aliases                                         # Complete aliased commands
       setopt correct                                                  # Auto correct mistakes
       setopt extendedglob                                             # Extended globbing. Allows using regular expressions with *
       setopt nocaseglob                                               # Case insensitive globbing
       setopt rcexpandparam                                            # Array expension with parameters
       setopt nocheckjobs                                              # Don't warn about running processes when exiting
       setopt numericglobsort                                          # Sort filenames numerically when it makes sense
-      setopt appendhistory                                            # Immediately append history instead of overwriting
-      setopt histignorealldups                                        # If a new command is a duplicate, remove the older one
-      setopt autocd                                                   # if only directory path is entered, cd there.
-      setopt inc_append_history                                       # save commands are added to the history immediately, otherwise only when shell exits.
-      setopt histignorespace                                          # Don't save commands that start with space
 
-      # completions config
+      # autocompletions config
       zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'       # Case insensitive tab completion
-      zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"         # Colored completion (different colors for dirs/files/etc)
+      zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"       # Colored completion (different colors for dirs/files/etc)
       zstyle ':completion:*' rehash true                              # automatically find new executables in path
       zstyle ':completion::complete:*' gain-privileges 1
       zstyle -e ':autocomplete:*:*' list-lines 'reply=( $(( LINES / 3 )) )'
+
+      # Cycle/enter autocomplete with tab
       bindkey              '^I' menu-select
       bindkey "$terminfo[kcbt]" menu-select
-      ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=244'  										  # make autosuggest a little brighter
 
-      ## Keybindings section
-      bindkey -e
-      bindkey '^[[7~' beginning-of-line                               # Home key
-      bindkey '^[[H' beginning-of-line                                # Home key
-      if [[ "''${terminfo[khome]}" != "" ]]; then
-        bindkey "''${terminfo[khome]}" beginning-of-line              # [Home] - Go to beginning of line
-      fi
-      bindkey '^[[8~' end-of-line                                     # End key
-      bindkey '^[[F' end-of-line                                      # End key
-      if [[ "''${terminfo[kend]}" != "" ]]; then
-        bindkey "''${terminfo[kend]}" end-of-line                     # [End] - Go to end of line
-      fi
-      bindkey '^[[2~' overwrite-mode                                  # Insert key
-      bindkey '^[[3~' delete-char                                     # Delete key
-      bindkey '^[[C'  forward-char                                    # Right key
-      bindkey '^[[D'  backward-char                                   # Left key
-      bindkey '^[[5~' history-beginning-search-backward               # Page up key
-      bindkey '^[[6~' history-beginning-search-forward                # Page down key
-
-      # Navigate words with ctrl+arrow keys
-      bindkey '^[Oc' forward-word                                     #
-      bindkey '^[Od' backward-word                                    #
-      bindkey '^[[1;5D' backward-word                                 #
-      bindkey '^[[1;5C' forward-word                                  #
+      # Navigate words
+      bindkey '^[[1;5D' backward-word                                 # Ctrl + right key
+      bindkey '^[[1;5C' forward-word                                  # Ctrl + left key
       bindkey '^H' backward-kill-word                                 # delete previous word with ctrl+backspace
-      bindkey '^[[Z' undo                                             # Shift+tab undo last action
-      WORDCHARS="*?[]~=&;!#$%^(){}<>"                                 # Let's break up words more
+      # bindkey '^[[Z' undo                                             # Shift+tab undo last action
 
       function nixpkgs() {
         NIXPKGS_ALLOW_UNFREE=1 nix shell "''${@/#/nixpkgs#}"
@@ -119,7 +126,6 @@
         NIXPKGS_ALLOW_UNFREE=1 nix run "nixpkgs#$PKG" -- $@
       }
 
-      PATH="$HOME/.deno/bin:$PATH"
 
       # foot integration
       function osc7-pwd() {
