ecdd81cbc29b1c153776cee57cd980fbd68434ab
ozwaldorf <self@ossian.dev>
2025-05-12 00:33:38 -0400

    chore: remove unused pkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,6 @@
 inputs: final: prev: {
   webcord = import ./webcord.nix prev;
-  xq = import ./xq.nix prev;
-  # zed-editor = import ./zed prev;
+  # xq = import ./xq.nix prev;
 
   neovim = import ./neovim.nix {
     inherit inputs;
