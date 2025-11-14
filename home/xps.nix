c820402c228c4ed9af535f03b249a900f31f1121
ozwaldorf <self@ossian.dev>
2025-11-13 23:33:21 -0500

    feat: port old_xps system configuration

---
+++ home/xps.nix
@@ -0,0 +1,28 @@
+{ pkgs, inputs, username, homeDirectory, ... }: {
+  imports = [
+    # carburetor theming
+    inputs.carburetor.homeManagerModules.default
+    inputs.vicinae.homeManagerModules.default
+
+    ./headless/zsh.nix # Shell
+    ./headless/starship.nix # Prompt
+    ./headless/git.nix # Git
+    ./headless/dev.nix # Dev utils
+
+    ./graphical/hyprland.nix # window manager
+    ./graphical/gtk.nix # gtk theming
+    ./graphical/ags # top bar
+    ./graphical/vicinae.nix # app launcher
+    ./graphical/foot.nix # Terminal
+    ./graphical/vesktop # discord
+    ./graphical/zed.nix # zed editor
+  ];
+
+  home = {
+    inherit username homeDirectory;
+    stateVersion = "24.05";
+    packages = with pkgs; [ lutgen beekeeper-studio prismlauncher ];
+  };
+
+  programs.home-manager.enable = true;
+}
---
+++ systems/xps/configuration.nix
@@ -0,0 +1,183 @@
+{ config, pkgs, username, hostname, ... }: {
+  imports = [ # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+  disabledModules = [ "hardware/facter/system.nix" ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  users.users.${username} = {
+    isNormalUser = true;
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh;
+    packages = with pkgs; [ firefox ];
+  };
+
+  networking = {
+    hostName = hostname;
+    networkmanager.enable = true;
+  };
+
+  time.timeZone = "America/New_York";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    plymouth = { enable = true; };
+    kernelPackages = pkgs.linuxPackages_latest;
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = [ "quiet" "splash" "udev.log_level=0" ];
+  };
+
+  hardware.opengl = {
+    enable = true;
+    driSupport32Bit = true;
+  };
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+  services.blueman.enable = true;
+
+  services.getty.autologinUser = username;
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "Hyprland";
+        user = username;
+      };
+    };
+  };
+
+  services.sshd.enable = true;
+
+  services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+  services.xserver.desktopManager.gnome.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+  services.xserver = {
+    xkb.layout = "us";
+    xkb.variant = "";
+  };
+
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    # extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+
+  services.printing.enable = true;
+  services.printing.drivers = [ pkgs.pantum-driver ];
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
+
+  security.rtkit.enable = true;
+  hardware.pulseaudio.enable = false;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+  security.polkit.enable = true;
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart =
+          "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+
+  # environment.etc = {
+  #   "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+  #     bluez_monitor.properties = {
+  #       ["bluez5.enable-sbc-xq"] = true,
+  #       ["bluez5.enable-msbc"] = true,
+  #       ["bluez5.enable-hw-volume"] = true,
+  #       ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+  #     }
+  #   '';
+  # };
+
+  fonts.packages = with pkgs; [ nerd-fonts.fira-code ];
+
+  programs.zsh.enable = true;
+  environment.pathsToLink = [ "/share/zsh" ];
+  fonts.fontconfig.enable = true;
+
+  programs.sway = {
+    enable = true;
+    package = pkgs.swayfx;
+    extraOptions = [ "--unsupported-gpu" ];
+  };
+  programs.hyprland.enable = true;
+
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  programs.direnv = {
+    enable = true;
+    direnvrcExtra = "export SHELL=$SHELL";
+  };
+
+  nix.settings.auto-optimise-store = true;
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+  nix.registry = {
+    nixpkgs.to = {
+      type = "path";
+      path = pkgs.path;
+    };
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  system.stateVersion = "24.05"; # Did you read the comment?
+}
