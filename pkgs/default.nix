273811328ebf3b5248b08a0dc28a5cf4f2783d8c
ozwaldorf <self@ossian.dev>
2025-11-14 20:01:47 -0500

    chore: remove custom webcord

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,5 @@
 inputs: final: prev: {
-  webcord = import ./webcord.nix prev;
   # xq = import ./xq.nix prev;
-
   neovim = import ./neovim.nix {
     inherit inputs;
     pkgs = prev;
@@ -10,7 +8,6 @@ inputs: final: prev: {
     inherit inputs;
     pkgs = final;
   };
-
   snd-hda-intel = prev.callPackage (import ./snd-hda-intel.nix) { };
   beammp-server = prev.callPackage (import ./beammp-server.nix) { };
 }
