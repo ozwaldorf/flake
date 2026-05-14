2e48eb8505584bf9413e7af59614d8d1c7340eb4
ozwaldorf <self@ossian.dev>
2026-05-14 12:26:49 -0400

    chore(guide): disable xterm desktop manager

--- systems/guide/configuration.nix
+++ systems/guide/configuration.nix
@@ -124,6 +124,7 @@
   services.xserver.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
   services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.xterm.enable = false;
   services.gnome.gnome-keyring.enable = true;
   services.xserver = {
     xkb.layout = "us";
